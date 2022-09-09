320fd575cb42b16d18cf65115ff4bf6a62dfd518
rafsun <rafsundipto116@gmail.com>
2022-09-09 22:00:50 +0600

    node-practice-font-end-react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { useEffect, useRef, useState } from 'react';
 
 function App() {
+  const [users, setUsers] = useState([]);
+  const nameRef = useRef();
+  const emailRef = useRef();
+ 
+  useEffect(() => {
+    fetch('http://localhost:5000/users')
+      .then(res => res.json())
+      .then(data => setUsers(data));
+  }, [])
+  
+  const handelAddUser =(e)=> {
+   
+    const name = nameRef.current.value;
+    const email = emailRef.current.value;
+    const newUser = { name: name, email: email }
+    
+    //send data to the server
+    fetch('http://localhost:5000/users', {
+      method: 'post',
+      headers: {
+        'content-type':'application/json'
+      },
+      body: JSON.stringify(newUser)
+    })
+      .then(res => res.json())
+      .then(data => {
+        console.log(data);
+        const addeduser = data;
+        const newUsers = [...users, addeduser];
+        setUsers(newUsers);
+    })
+    nameRef.current.value = '';
+    emailRef.current.value = '';
+     e.preventDefault();
+  }
+
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <dir>found users: {users.length}</dir>
+      <form onSubmit={handelAddUser}>
+        <input type="text" ref={nameRef} placeholder='name' />
+        <input type="email" ref={emailRef} placeholder='emailAddress' />
+        <input type="submit" value='submit' />
+</form>
+
+      <ul>
+        {
+          users.map(user => <li key={user.id}>
+            {user.id}. {user.name} {user.email}
+          </li>)
+        }
+     </ul>
     </div>
   );
 }
